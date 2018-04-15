4f6551ef9438eabda9d276a0e0aba94b7acfc005
masagi94 <masagi94@gmail.com>
2018-04-15 16:13:44 -0400

    .

--- app/src/main/java/gmu/shakesafe/MainActivity.java
+++ app/src/main/java/gmu/shakesafe/MainActivity.java
@@ -1,20 +1,17 @@
 package gmu.shakesafe;
 
-//for sensor
+
 import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
-import android.os.AsyncTask;
 import android.os.Handler;
 import android.os.PowerManager;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
-import android.support.v7.app.AlertDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,7 +21,6 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.Vibrator;
 import android.widget.TextView;
 //for sensor
 
@@ -536,116 +532,4 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
-//    public class AnalyzeSensorData extends AsyncTask<SensorEvent, Void, Void> {
-//
-//        @Override
-//        public Void doInBackground(SensorEvent... ev) {
-//            PowerManager pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
-//            SensorEvent event = ev[0];
-//
-//            boolean isScreenOn;
-//
-//            try {
-//                isScreenOn = pm.isInteractive();
-//            } catch (NullPointerException e) {
-//                isScreenOn = false;
-//            }
-//
-//            // Only updates the sensor values if we're on the sensors tab, or the screen is off.
-//            // This will avoid slowing down the system when the app is running in the background
-//            // and the phone is being used.
-//            if (mViewPager.getCurrentItem() == 2 || !isScreenOn) {
-//                double newAcc, tilt, denom, deltaX, deltaY, deltaZ;
-//
-//                double realX, realY, realZ;
-//
-//                double gravityX = accSensor.getGravityX();
-//                double gravityY = accSensor.getGravityY();
-//                double gravityZ = accSensor.getGravityZ();
-//
-//
-//                final double alpha = 0.8;
-//                //these values will accommodate for gravitational acceleration
-//
-//                gravityX = alpha * gravityX + (1 - alpha) * event.values[0];
-//                gravityY = alpha * gravityY + (1 - alpha) * event.values[1];
-//                gravityZ = alpha * gravityZ + (1 - alpha) * event.values[2];
-//
-//
-//
-//                realX = Math.abs((float) (event.values[0] - gravityX));
-//                realY = Math.abs((float) (event.values[1] - gravityY));
-//                realZ = Math.abs((float) (event.values[2] - gravityZ));
-//                //end gravity code
-//
-//                // get the change of the x,y,z values of the accelerometer
-////                accSensor.setDeltaX((Math.abs(0 - event.values[0])));
-////                accSensor.setDeltaY((Math.abs(0 - event.values[1])));
-////                accSensor.setDeltaZ((Math.abs(0 - event.values[2])));
-//                deltaX = (Math.abs(0 - event.values[0]));
-//                deltaY = (Math.abs(0 - event.values[1]));
-//                deltaZ = (Math.abs(0 - event.values[2]));
-//
-//                // if the change is below 1, it is considered noise
-//                if (deltaX < 1)
-//                    deltaX = 0;
-//                if (deltaY < 1)
-//                    deltaY = 0;
-//                if (deltaZ < 1)
-//                    deltaZ = 0;
-//
-//                // deriving tilt
-//                denom = (float) Math.sqrt((deltaX * deltaX) + (deltaY * deltaY) + (deltaZ * deltaZ));
-//                tilt = (float) Math.acos(deltaZ / denom);
-//                tilt = (float) ((tilt * 180) / 3.14);
-//
-//                newAcc = Math.sqrt(realX * realX + realY * realY + realZ * realZ);
-//
-//
-//
-//
-//                // Calculates the standard deviation on a background thread
-//                new CalculateSD().execute(newAcc);
-//
-//                if (tilt < 1 && !isScreenOn) {
-//
-//                    if (UPLOADS_ON) {
-//
-//                        // This creates a delay so the threshold isn't triggered when the phone's screen
-//                        // is turned off manually
-//                        if (!screenDelayStarted) {
-//
-//                            screenDelayStarted = true;
-//                            screenOffTimer();
-//                            Log.d(LOG_TAG, "SCREEN OFF TIMER: STARTED");
-//
-//                        } else if (canUpload && (newAcc >= sdObject.getThreshold()) && screenDelayDone) {
-//
-//                            canUpload = false;
-//                            storeData(0, 0, 0);
-//
-//                            new UploadData().execute(getApplicationContext());
-//                            Log.d(LOG_TAG, "UPLOADING...");
-//                        }
-//                    } else
-//                        Log.d(LOG_TAG, "******** UPLOADS ARE DISABLED ********");
-//                }
-//
-//
-//
-//                // Local calculations are done. Store values back into object for later use.
-//                accSensor.setDeltaX(deltaX);
-//                accSensor.setDeltaY(deltaY);
-//                accSensor.setDeltaZ(deltaZ);
-//
-//                accSensor.setGravityX(gravityX);
-//                accSensor.setGravityY(gravityY);
-//                accSensor.setGravityZ(gravityZ);
-//
-//                accSensor.setTilt(tilt);
-//
-//            }
-//            return null;
-//        }
-//    }
 }
--- app/src/main/java/gmu/shakesafe/SensorObject.java
+++ app/src/main/java/gmu/shakesafe/SensorObject.java
@@ -7,7 +7,6 @@ package gmu.shakesafe;
 public class SensorObject {
     private double gravityX, gravityY, gravityZ = 0;
     private double deltaX, deltaY, deltaZ = 0;
-    //private double realX, realY, realZ = 0;
     private double tilt = 0;
 
 
@@ -15,82 +14,73 @@ public class SensorObject {
     public SensorObject(){}
 
     public double getGravityX() {
+
         return gravityX;
     }
 
-    public void setGravityX(double gravX) {
+    public void setGravityX(double gravityX) {
+
         this.gravityX = gravityX;
     }
 
     public double getGravityY() {
+
         return gravityY;
     }
 
     public void setGravityY(double gravitY) {
+
         this.gravityY = gravitY;
     }
 
     public double getGravityZ() {
+
         return gravityZ;
     }
 
     public void setGravityZ(double gravityZ) {
+
         this.gravityZ = gravityZ;
     }
 
     public double getDeltaX() {
+
         return deltaX;
     }
 
     public void setDeltaX(double deltaX) {
+
         this.deltaX = deltaX;
     }
 
     public double getDeltaY() {
+
         return deltaY;
     }
 
     public void setDeltaY(double deltaY) {
+
         this.deltaY = deltaY;
     }
 
     public double getDeltaZ() {
+
         return deltaZ;
     }
 
     public void setDeltaZ(double deltaZ) {
+
         this.deltaZ = deltaZ;
     }
 
-//    public double getRealX() {
-//        return realX;
-//    }
-//
-//    public void setRealX(double realX) {
-//        this.realX = realX;
-//    }
-//
-//    public double getRealY() {
-//        return realY;
-//    }
-//
-//    public void setRealY(double realY) {
-//        this.realY = realY;
-//    }
-//
-//    public double getRealZ() {
-//        return realZ;
-//    }
-//
-//    public void setRealZ(double realZ) {
-//        this.realZ = realZ;
-//    }
 
     public double getTilt() {
+
         return tilt;
     }
 
     public void setTilt(double tilt) {
+
         this.tilt = tilt;
     }
 }
